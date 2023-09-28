4076d4141caba52ffb5a946538c7a02daa8230ea
Resin-code <greiner.roli@gmail.com>
2023-09-28 18:11:15 +0200

    task: 4-division

---
+++ osztas_5tel_3mal.py
@@ -0,0 +1,6 @@
+sum = int(input("Írjon be egy számot: "))
+
+if sum % 3 == 0 or sum % 5 == 0:
+    print("A szám osztható 3-mal vagy 5-tel.")
+else:
+    print("A szám nem osztható 3-mal vagy 5-tel.")
